8c901e174afcfebe170baad2a239c98a8a0cf2bf
Lyra <63160185+ejlee127@users.noreply.github.com>
2020-08-27 15:17:27 -0400

    update ERD and schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-
+--DROP TABLE "State_Abb";
 CREATE TABLE "State_Abb" (
     "State_id" int   NOT NULL,
     "State" varchar(30)   NOT NULL,
@@ -12,6 +12,7 @@ CREATE TABLE "State_Abb" (
      )
 );
 
+--DROP TABLE "NaturalGas_Production";
 CREATE TABLE "NaturalGas_Production" (
     "State_id" int   NOT NULL,
     "Rank" int   NOT NULL,
@@ -21,6 +22,7 @@ CREATE TABLE "NaturalGas_Production" (
      )
 );
 
+--DROP TABLE "Energy_Production_Estimate";
 CREATE TABLE "Energy_Production_Estimate" (
     "State_id" int   NOT NULL,
     "Fossil_Fuels" int   NOT NULL,
@@ -30,13 +32,15 @@ CREATE TABLE "Energy_Production_Estimate" (
     "Biofuels" int   NOT NULL,
     "Wood_Waste" int   NOT NULL,
     "Other" int   NOT NULL,
+	"Total" int,
     CONSTRAINT "pk_Energy_Production_Estimate" PRIMARY KEY (
         "State_id"
      )
 );
 
+--DROP TABLE "Renewable_Energy_Potential";
 CREATE TABLE "Renewable_Energy_Potential" (
-    "State_id" int,   NOT NULL,
+    "State_id" int   NOT NULL,
     "urbanUtilityScalePV_GWh" int   NOT NULL,
     "ruralUtilityScalePV_GWh" int   NOT NULL,
     "rooftopPV_GWh" int   NOT NULL,
@@ -53,12 +57,17 @@ CREATE TABLE "Renewable_Energy_Potential" (
      )
 );
 
+--DROP TABLE "Electricity_Production";
 CREATE TABLE "Electricity_Production" (
-    "State_id" PKint   NOT NULL,
+    "State_id" int  NOT NULL,
     "Rank" int   NOT NULL,
-    "Total_Net_GWh" int   NOT NULL
+    "Total_Net_GWh" int   NOT NULL,
+	CONSTRAINT "pk_Electricity_Production" PRIMARY KEY (
+        "State_id"
+     )
 );
 
+--DROP TABLE "Coal_Production";
 CREATE TABLE "Coal_Production" (
     "State_id" int   NOT NULL,
     "Rank" int   NOT NULL,
@@ -68,6 +77,7 @@ CREATE TABLE "Coal_Production" (
      )
 );
 
+--DROP TABLE "Crudeoil_Production";
 CREATE TABLE "Crudeoil_Production" (
     "State_id" int   NOT NULL,
     "Rank" int   NOT NULL,
@@ -77,6 +87,7 @@ CREATE TABLE "Crudeoil_Production" (
      )
 );
 
+--DROP TABLE "Total_Energy_Consumed";
 CREATE TABLE "Total_Energy_Consumed" (
     "State_id" int   NOT NULL,
     "Rank" int   NOT NULL,
@@ -86,6 +97,7 @@ CREATE TABLE "Total_Energy_Consumed" (
      )
 );
 
+--DROP TABLE "Total_Energy_Production";
 CREATE TABLE "Total_Energy_Production" (
     "State_id" int   NOT NULL,
     "Rank" int   NOT NULL,
@@ -94,4 +106,3 @@ CREATE TABLE "Total_Energy_Production" (
         "State_id"
      )
 );
-
